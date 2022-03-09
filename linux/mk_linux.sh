ae579a9954ef323afc4108496d891178beabb2c5
Tucker Osman <osmiumusa@gmail.com>
2022-03-09 01:14:51 -0600

    trying to use GCC 4.6

--- linux/mk_linux.sh
+++ linux/mk_linux.sh
@@ -6,13 +6,10 @@ cd "${SCRIPT_DIR}"
 VERSION="2.6.39"
 CONFIG="linux-${VERSION}.config"
 
-docker build . -t atv-kernel-builder
-
 if [ ! -f linux-${VERSION}.tar.bz2 ] ; then
 	wget http://www.kernel.org/pub/linux/kernel/v2.6/linux-${VERSION}.tar.bz2
 fi
-#
-#
+
 tar -jxf linux-${VERSION}.tar.bz2
 
 if [ -f "${CONFIG}" ] ; then
@@ -22,9 +19,9 @@ else
 fi
 #
 cd  linux-${VERSION}
-patch -p1 < ../atv-kernel.patch
-docker run --rm -it -v $(pwd):/build atv-kernel-builder make oldconfig
-docker run --rm -it -v $(pwd):/build atv-kernel-builder make -j$(nproc --all)
+
+docker run --rm -it -v $(pwd):/build -w=/build gcc:4.6 make oldconfig
+docker run --rm -it -v $(pwd):/build -w=/build gcc:4.6 make -j$(nproc --all)
 
 #
 cd "${SCRIPT_DIR}"
